ccf55832402e35a82dc1e5f5ff7ccf1e378fecae
julianty <alexanderjulianty@gmail.com>
2022-10-21 11:22:01 -0700

    Adds styling to resume page

--- src/ResumePage.js
+++ src/ResumePage.js
@@ -1,98 +1,111 @@
-import { Container, Row, Col, Image, ListGroup} from 'react-bootstrap';
+import { Container, Row, Col, Image, Card, Link} from 'react-bootstrap';
 import ResumePhoto from './images/meInSuitCropped.jpg'
 
 function ExperienceFormat(props) {
   function subtitle() {
     if(props.subtitle === undefined) return;
     return (
-      <Row>
-        <Col>
-          <p><em>{props.subtitle}</em></p>
-        </Col>
-        <Col>
-          <p><em>{props.company}</em></p>
-        </Col>
-      </Row>
+      <Container className='d-flex justify-content-between'>
+        <p><em>{props.subtitle}</em></p>
+        <p><em>{props.company}</em></p>
+      </Container>
     );
   }
   function content() {
     if(props.text !== undefined) return <p>{props.text}</p>
     if(props.list !== undefined) {
       return (
-        <ListGroup>
+        <ul>
           {props.list.map(item => {
             return (
-              <ListGroup.Item>{item}</ListGroup.Item>
+              <li>{item}</li>
             )
           })}
-        </ListGroup>
+        </ul>
       )
       // console.log(props.list);
     }
   }
   return (
-    <>
-      <Row>
-        <Col>
-          <p><strong>{props.title}</strong></p>
-        </Col>
-        <Col>
-          <p><em>{props.dates}</em></p>
-        </Col>
-      </Row>
-      {subtitle()}
-      <Row>
-        {content()}
-      </Row>
-    </>
+    <Card className='col-9'>
+      <Card.Header as='h6' className='d-flex justify-content-between'>
+        <div>
+          {props.title}
+        </div>
+        <div>
+          {props.dates}
+        </div>
+      </Card.Header>
+      <Card.Body>
+        <Card.Text>
+          {subtitle()}
+          {content()}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   )
 }
 
 function ResumePage() {
   return (
-    <>
-      <Container>
-        <Row>
-          <Col className='col-2'>
-            <Image src={ResumePhoto} fluid/>
-          </Col>
-          <Col className='col-7'>
-            <Row>
-              <h2>Alexander Julian A. Ty</h2>
-            </Row>
-            <Row>
-              <Col className='col-4'>
-                <p>alexanderjulianty@gmail.com</p>
-                <p>(858) 281-3589</p>
-              </Col>
-              <Col className='col-4'>
-                <p>5360 Toscana Way, G109, San Diego, CA, 92122</p>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
+    <Container>
+      <Row>
+        <Col className='d-flex justify-content-center'>
+          <Card className='col-7'>
+            <Card.Body>
+              <Row>
+                <Col className='col-3'>
+                  <Image src={ResumePhoto} fluid/>
+                </Col>
+                <Col className='col-9'>
+                  <Row>
+                    <h4>Alexander Julian A. Ty</h4>
+                  </Row>
+                  <Row>
+                    <Card.Link href='mailto:alexanderjulianty@gmail.com'>alexanderjulianty@gmail.com</Card.Link>
+                    <p>(858) 281-3589</p>
+                    <p>5360 Toscana Way, G109, San Diego, CA, 92122</p>
+                    <a href="https://drive.google.com/file/d/1mfgaA4uw9m8Aq3JhR6XGwM4k4eu3MkUa/view?usp=sharing">PDF version</a>
+                  </Row>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
 
-        <Row>
-          <h3>Purpose</h3>
-          <p>
+      <Row className='offset-1'>
+        <h3>Purpose</h3>
+      </Row>
+      <Row className='d-flex justify-content-center'>
+        <Card className='col-9'>
+          <Card.Body>
             As I believe my strength lies in working others, I am in 
             pursuit of a challenging work environment with opportunities to 
-            collaborate in groups, preferably with an emphasis on programming.
-            Also, having opportunities for mentorship is important to me, 
-            being both a mentor and mentee.
-          </p>
-        </Row>
+            collaborate in groups, preferably with an emphasis on 
+            programming.Also, having opportunities for mentorship is 
+            important to me, being either or both a mentor and mentee.
+          </Card.Body>
+        </Card>
+      </Row>
 
-        <Row>
-          <h3>Education</h3>
+      <Row className='offset-1'>
+        <h3>Education</h3>
+      </Row>
+      <Row> 
+        <Col className='d-flex justify-content-center'>
           <ExperienceFormat 
             title='University of California San Diego'
             dates='June 2015'
             text='B.S. in Engineering Physics: Semiconductor Physics'
           />
-        </Row>
-        <Row>
-          <h3>Experience</h3>
+        </Col>
+      </Row>
+      <Row className='offset-1'>
+        <h3>Experience</h3>
+      </Row>
+      <Row>
+        <Col className='d-flex justify-content-center'>
           <ExperienceFormat 
             title='Underwater Optical Communication'
             dates='January 2015 - June 2015'
@@ -104,6 +117,10 @@ function ResumePage() {
               "Explored a proof of concept for utilizing network media converters as a substitute for optical transceiver modules."
             ]} 
           />
+        </Col>
+      </Row>
+      <Row>
+        <Col className='d-flex justify-content-center'>
           <ExperienceFormat 
             title='Office of Academic Support and Instructional Services'
             dates='September 2016 - June 2018'
@@ -116,6 +133,10 @@ function ResumePage() {
               "Used Python, R, Excel for presenting and analyzing data"
             ]} 
           />
+        </Col>
+      </Row>
+      <Row>
+        <Col className='d-flex justify-content-center'>
           <ExperienceFormat 
             title='Staff Research Associate'
             dates='June 2018 - June 2020'
@@ -126,29 +147,38 @@ function ResumePage() {
               "Used Python, C, SSH (for the use of UCSD's CPU array)"
             ]} 
           />
-        </Row>
+        </Col>
+      </Row>
 
-        <Row>
-          <h3>Technical Strengths</h3>
-          <Row>
-            <Col className='col-3'>
-              <p><strong>Proficient In</strong></p>
-            </Col>
-            <Col className='col-6'>
-              <p>C, Python, LaTeX, R, Microsoft Office Suite, HTML, CSS, Javascript</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col className='col-3'>
-              <p><strong>Working knowledge of</strong></p>
-            </Col>
-            <Col className='col-6'>
-              <p>MATLAB, SolidWorks, LabVIEW, SQL, Octave</p>
-            </Col>
-          </Row>
-        </Row>
-      </Container>
-    </>
+      <Row className='offset-1'>
+        <h3>Technical Strengths</h3>
+      </Row>
+      <Row>
+        <Col className='d-flex justify-content-center'>
+          <Card className='col-9'>
+            <Card.Body>
+              <Row>
+                <Col>
+                  <p>Proficient In:</p>
+                </Col>
+                <Col>
+                  <p>C, Python, LaTeX, R, Microsoft Office Suite, HTML, CSS, Javascript</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <p>Working knowledge of</p>
+                </Col>
+                <Col>
+                  <p>MATLAB, SolidWorks, LabVIEW, SQL, Octave</p>
+                </Col>
+              </Row>
+            </Card.Body>
+
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
