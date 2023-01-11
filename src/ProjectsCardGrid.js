2913c9e6fa3a1db04e07cca9515dbb4eacead551
julianty <alexanderjulianty@gmail.com>
2023-01-11 10:02:35 -0800

    Removes unnecessary logo from chicken feet card

--- src/ProjectsCardGrid.js
+++ src/ProjectsCardGrid.js
@@ -3,45 +3,42 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 
-import { SocialLinks, LOGOS} from "./SocialLinks";
+import { SocialLinks, LOGOS } from "./SocialLinks";
 
-import chickenFeetThumbnail from './images/chickenFeetThumbnail.png';
-import chickenToDoThumbnail from './images/chickenToDoThumbnail.png';
-import MLPaperThumbnail from './images/MLPaperThumbnail.png';
+import chickenFeetThumbnail from "./images/chickenFeetThumbnail.png";
+import chickenToDoThumbnail from "./images/chickenToDoThumbnail.png";
+import MLPaperThumbnail from "./images/MLPaperThumbnail.png";
 
 function ProjectCard(props) {
   const onMouseEnter = (e) => {
-    e.currentTarget.classList.add('shadow');
-  }
+    e.currentTarget.classList.add("shadow");
+  };
   const onMouseLeave = (e) => {
-    e.currentTarget.classList.remove('shadow');
-  }
+    e.currentTarget.classList.remove("shadow");
+  };
   const clickHandler = (e) => {
-    window.open(props.href, '_blank');
-  }
+    window.open(props.href, "_blank");
+  };
   return (
-    <Card 
-      onMouseEnter={onMouseEnter} 
-      onMouseLeave={onMouseLeave} 
-      onClick={clickHandler} 
-      style={{cursor: 'pointer'}}
+    <Card
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={clickHandler}
+      style={{ cursor: "pointer" }}
     >
-      <Card.Img src={props.imgSrc} fluid/>
+      <Card.Img src={props.imgSrc} fluid />
       <Card.Body>
-        <Card.Title className='d-flex justify-content-between'>
+        <Card.Title className="d-flex justify-content-between">
           {props.title}
           <div>
-            <SocialLinks logo={props.logo} href={props.href}/>
+            <SocialLinks logo={props.logo} href={props.href} />
           </div>
         </Card.Title>
-        <Card.Text>
-          {props.text}
-        </Card.Text>
+        <Card.Text>{props.text}</Card.Text>
       </Card.Body>
     </Card>
-  )
+  );
 }
-
 
 function ProjectsCardGrid() {
   return (
@@ -52,40 +49,40 @@ function ProjectsCardGrid() {
       <Row>
         <Col className="col-3">
           <ProjectCard
-            title='Chicken Feet'
+            title="Chicken Feet"
             text="My first attempt at developing a game in Unity as a part of 
                 the 'USC Games Unlocked' Course"
             imgSrc={chickenFeetThumbnail}
+            logo=""
           />
         </Col>
         <Col className="col-3">
-          <ProjectCard 
-            title='Chicken To Do' 
+          <ProjectCard
+            title="Chicken To Do"
             text="Made with React and using Firebase from google
               as a BaaS, it is an attempt at emulating task trackers such as
               Milanote and Asana. Currently a work in progress"
             imgSrc={chickenToDoThumbnail}
             logo={LOGOS.GITHUB}
-            href='https://github.com/julianty/chicken-to-do'
+            href="https://github.com/julianty/chicken-to-do"
           />
         </Col>
         <Col className="col-3">
-          <ProjectCard 
-            title='ML Research Paper' 
+          <ProjectCard
+            title="ML Research Paper"
             text="An analysis of an intersection between Information Theory, 
               Data Assimilation, and a particular method of training a neural
               network. Examines the possibility of determining a theoretically
               sufficient number of training examples to achieve a desired level
-              of accuracy instead of an 'as much as possible' approach." 
+              of accuracy instead of an 'as much as possible' approach."
             imgSrc={MLPaperThumbnail}
             logo={LOGOS.ARXIV}
-            href='https://arxiv.org/abs/1902.05062'
+            href="https://arxiv.org/abs/1902.05062"
           />
         </Col>
-
       </Row>
     </Container>
-  )
+  );
 }
 
 export default ProjectsCardGrid;
