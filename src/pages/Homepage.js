6d6db3c78f396641c44cb8d3ac9dfdb4c26afd74
julianty <alexanderjulianty@gmail.com>
2023-09-21 15:43:59 -0700

    Adds project highlights to home page

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,12 +4,18 @@ import Col from "react-bootstrap/Col";
 import Stack from "react-bootstrap/Stack";
 import Image from "react-bootstrap/Image";
 
-import AboutMeCard from "../AboutMeCard";
+import AboutMeCard from "../components/AboutMeCard";
+import ProjectTag from "../components/ProjectTag";
+import ProjectHighlight from "../components/ProjectHighlight";
+import { SocialLinks, LOGOS } from "../components/SocialLinks";
+
 import meInSuit from "../images/meInSuitSquare.jpg";
+import toDoThumbnail from "../images/toDoProjectThumbnail.png";
+import workoutTrackerThumbnail from "../images/workoutTrackerThumbnail.png";
 function Homepage() {
   return (
     <Container>
-      <Container className="p-3 my-5">
+      <Container className="p-3 my-3">
         <Row className="justify-content-center align-items-center">
           <Col className="col-3 px-0 shadow">
             <Image src={meInSuit} fluid></Image>
@@ -19,13 +25,50 @@ function Homepage() {
           </Col>
         </Row>
       </Container>
+
       <Container>
         <Row className="justify-content-center">
-          <Col>
-            Welcome to my website. Please use the navigation bar above to look
-            around!
-          </Col>
+          <h2>Project Highlights</h2>
         </Row>
+        <ProjectHighlight
+          title={"To Do Web Application"}
+          tags={["REACT", "FIREBASE"]}
+          description={
+            "A work in progress for improving my productivity, this web application is built with React. This project is hosted on and utilizes cloud storage via Google Firebase."
+          }
+          imageSrc={toDoThumbnail}
+          imageHref={"https://chicken-tasks.firebaseapp.com/"}
+          socialLinks={{
+            github: "https://github.com/julianty/chicken-to-do",
+            web: "https://chicken-tasks.firebaseapp.com/",
+          }}
+        />
+        <ProjectHighlight
+          title={"Workout Tracker"}
+          tags={["REACT", "FIREBASE"]}
+          description={
+            "A simple workout tracker project built with react and utilizing Redux for state management. Hosted and utilizes storage on google Firebase."
+          }
+          imageSrc={workoutTrackerThumbnail}
+          imageHref={"https://workout-tracker-2d69d.firebaseapp.com/"}
+          socialLinks={{
+            github: "https://github.com/julianty/workout-tracker",
+            web: "https://workout-tracker-2d69d.firebaseapp.com/",
+          }}
+        />
+        <ProjectHighlight
+          title={"Sentence Per Day"}
+          tags={["REACT", "MONGOOSE"]}
+          description={
+            "Learning a new language has been on my to-do list for a long time, and this application is meant to build a habit of translating a single sentence each day from the target language to English."
+          }
+          // imageSrc={toDoThumbnail}
+          imageHref={"https://chicken-tasks.firebaseapp.com/"}
+          socialLinks={{
+            github: "https://github.com/julianty/chicken-to-do",
+            web: "https://chicken-tasks.firebaseapp.com/",
+          }}
+        />
       </Container>
     </Container>
   );
