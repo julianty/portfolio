4be1ac490b6d5fb18fbb6ce3da8452baa3a7932d
julianty <alexanderjulianty@gmail.com>
2023-11-30 16:27:29 -0800

    Implements a design overhaul for highlights

--- src/components/ProjectHighlight.js
+++ src/components/ProjectHighlight.js
@@ -20,68 +20,22 @@ export default function ProjectHighlight({
     imageHref: string(hyperlink)
     socialLinks: {LOGOS: string(hyperlink)}
   */
-  if (mirror) {
-    return (
-      <Container className="my-3 px-5">
-        <Row>
-          <Col xs={"auto"} className="d-flex align-items-center ms-auto">
-            {tags.map((tagName) => {
-              console.log(tagName);
-              return <ProjectTag tagLabel={tagName} key={uniqid()} />;
-            })}
-          </Col>
-          <Col xs={"auto"}>
-            <h3>{title}</h3>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={9}>
-            <Row>{description}</Row>
+  return (
+    <Container className="my-3 p-5 border rounded">
+      <Row>
+        <Col xs={7}>
+          <Row>
             <Row>
               <Col>
-                {Object.keys(socialLinks).map((logo) => {
-                  return (
-                    <SocialLinks
-                      logo={logo}
-                      href={socialLinks[logo]}
-                      key={uniqid()}
-                    />
-                  );
+                {tags.map((tagName) => {
+                  console.log(tagName);
+                  return <ProjectTag tagLabel={tagName} key={uniqid()} />;
                 })}
               </Col>
             </Row>
-          </Col>
-          <Col xs={3}>
-            <a href={imageHref} target="_blank" rel="noreferrer noopener">
-              <Image src={imageSrc} thumbnail></Image>
-            </a>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-
-  return (
-    <Container className="my-3 px-5">
-      <Row>
-        <Col xs={"auto"}>
-          <h3>{title}</h3>
-        </Col>
-        <Col className="d-flex align-items-center">
-          {tags.map((tagName) => {
-            console.log(tagName);
-            return <ProjectTag tagLabel={tagName} key={uniqid()} />;
-          })}
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={3}>
-          <a href={imageHref} target="_blank" rel="noreferrer noopener">
-            <Image src={imageSrc} thumbnail></Image>
-          </a>
-        </Col>
-        <Col xs={9}>
-          <Row>{description}</Row>
+            <h3>{title}</h3>
+          </Row>
+          {description}
           <Row>
             <Col>
               {Object.keys(socialLinks).map((logo) => {
@@ -95,6 +49,11 @@ export default function ProjectHighlight({
               })}
             </Col>
           </Row>
+        </Col>
+        <Col xs={5}>
+          <a href={imageHref} target="_blank" rel="noreferrer noopener">
+            <Image src={imageSrc} thumbnail></Image>
+          </a>
         </Col>
       </Row>
     </Container>
