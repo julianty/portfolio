6d4fbe6454422a652526854e15406629b4cea1a9
julianty <alexanderjulianty@gmail.com>
2023-11-30 16:27:12 -0800

    Redesigns about me card

--- src/components/AboutMeCard.js
+++ src/components/AboutMeCard.js
@@ -1,19 +1,31 @@
 import Card from "react-bootstrap/Card";
+import { Row, Col } from "react-bootstrap";
 import { SocialLinks, LOGOS } from "./SocialLinks";
 
 function AboutMeCard() {
   return (
     <Card>
       <Card.Body>
-        <Card.Title>About Me</Card.Title>
-        <Card.Text>
+        <Card.Title>Alexander Julian Ty</Card.Title>
+        {/* <Card.Text>
           San Diego based web-developer, amateur powerlifter, coffee enthusiast,
           avid gamer, aspiring game developer, former research scientist,
           hobbyist artist.
+        </Card.Text> */}
+        <Card.Text className="mb-0">
+          Full-stack Web Developer, UI/UX Designer
         </Card.Text>
-      </Card.Body>
-      <Card.Body>
-        <SocialLinks />
+        <Card.Text className="mb-0">Based in San Diego, California</Card.Text>
+        <Row>
+          <Col>
+            <Card.Text>
+              <a href="mailto:alexanderjulianty@gmail.com">Send me an email</a>
+            </Card.Text>
+          </Col>
+          <Col>
+            <SocialLinks />
+          </Col>
+        </Row>
       </Card.Body>
     </Card>
   );
