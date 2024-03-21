c89204d7afd10ae6780a29341aa7b113e02d6dbd
julianty <alexanderjulianty@gmail.com>
2024-03-20 17:35:31 -0700

    Rewrites about me card

--- src/components/AboutMeCard.js
+++ src/components/AboutMeCard.js
@@ -1,33 +1,24 @@
-import Card from "react-bootstrap/Card";
-import { Row, Col } from "react-bootstrap";
+// import Card from "react-bootstrap/Card";
+import { Text, Paper, Anchor, Title, Center, Group } from "@mantine/core";
 import { SocialLinks, LOGOS } from "./SocialLinks";
 
 function AboutMeCard() {
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>Alexander Julian Ty</Card.Title>
-        {/* <Card.Text>
-          San Diego based web-developer, amateur powerlifter, coffee enthusiast,
-          avid gamer, aspiring game developer, former research scientist,
-          hobbyist artist.
-        </Card.Text> */}
-        <Card.Text className="mb-0">
-          Full-stack Web Developer, UI/UX Designer
-        </Card.Text>
-        <Card.Text className="mb-0">Based in San Diego, California</Card.Text>
-        <Row>
-          <Col>
-            <Card.Text>
-              <a href="mailto:alexanderjulianty@gmail.com">Send me an email</a>
-            </Card.Text>
-          </Col>
-          <Col>
-            <SocialLinks />
-          </Col>
-        </Row>
-      </Card.Body>
-    </Card>
+    <Paper m="md" p="lg">
+      <Title order={2}>Alexander Julian Ty</Title>
+      <Text fw={500}> Full-stack Developer, Product Designer</Text>
+      {/* <Text>Based in San Diego, California</Text> */}
+      <Text c={"gray"} size="sm">
+        Hi, I'm Julian. I love making things and sharing with the world. If you
+        do too, then let's talk
+      </Text>
+      <Group justify="space-between">
+        <Anchor href="mailto:alexanderjulianty@gmail.com">
+          Send me an email
+        </Anchor>
+        <SocialLinks />
+      </Group>
+    </Paper>
   );
 }
 
