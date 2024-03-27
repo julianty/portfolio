2c3d3acca995199b46d2affb526a249e22b4be4f
julianty <alexanderjulianty@gmail.com>
2024-03-26 18:09:47 -0700

    Adds new icons to object

--- src/components/StyledIcons.js
+++ src/components/StyledIcons.js
@@ -3,9 +3,11 @@ import {
   IconBrandFirebase,
   IconBrandHtml5,
   IconBrandJavascript,
+  IconBrandMantine,
   IconBrandMongodb,
   IconBrandPython,
   IconBrandReact,
+  IconBrandRedux,
   IconBrandTypescript,
   icons,
 } from "@tabler/icons-react";
@@ -19,42 +21,62 @@ const iconStyles = {
 export const styledIcons = {
   html: (
     <IconBrandHtml5
+      key={"html5"}
       style={{ ...iconStyles, color: "var(--mantine-color-orange-6)" }}
     />
   ),
   css: (
     <IconBrandCss3
+      key={"css3"}
       style={{ ...iconStyles, color: "var(--mantine-color-blue-9)" }}
     />
   ),
   javascript: (
     <IconBrandJavascript
+      key={"javascript"}
       style={{ ...iconStyles, color: "var(--mantine-color-yellow-3)" }}
     />
   ),
   typescript: (
     <IconBrandTypescript
+      key={"typescript"}
       style={{ ...iconStyles, color: "var(--mantine-color-blue-5)" }}
     />
   ),
   python: (
     <IconBrandPython
+      key={"python"}
       style={{ ...iconStyles, color: "var(--mantine-color-blue-8)" }}
     />
   ),
   react: (
     <IconBrandReact
+      key={"react"}
       style={{ ...iconStyles, color: "var(--mantine-color-blue-3)" }}
     />
   ),
   firebase: (
     <IconBrandFirebase
+      key={"firebase"}
       style={{ ...iconStyles, color: "var(--mantine-color-orange-4)" }}
     />
   ),
   mongodb: (
     <IconBrandMongodb
+      key={"mongodb"}
       style={{ ...iconStyles, color: "var(--mantine-color-green-6)" }}
+    />
+  ),
+  mantine: (
+    <IconBrandMantine
+      key={"mantine"}
+      style={{ ...iconStyles, color: "var(--mantine-color-blue-filled" }}
+    />
+  ),
+  redux: (
+    <IconBrandRedux
+      key={"redux"}
+      style={{ ...iconStyles, color: "var(--mantine-color-violet-8" }}
     />
   ),
 };
