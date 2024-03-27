198316cb96fd27541575c829aebd9bba50957408
julianty <alexanderjulianty@gmail.com>
2024-03-26 18:10:10 -0700

    Adds route to corpore sano

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,12 +7,13 @@ import Container from "react-bootstrap/Container";
 import PortfolioPage from "./PortfolioPage";
 import ResumePage from "./ResumePage";
 import Homepage from "./Homepage";
+import CorporeSanoPage from "./CorporeSanoPage";
 import { Box, Center } from "@mantine/core";
 
 function LandingPage() {
   return (
     <Center>
-      <Box w={{ base: "100%", lg: "50%" }}>
+      <Box w={{ base: "100%", lg: "50%", sm: "75%" }}>
         <MantineNavbar />
 
         <Container fluid className="px-0">
@@ -21,6 +22,10 @@ function LandingPage() {
             <Route path="/Portfolio" element={<PortfolioPage />}></Route>
             <Route path="/Resume" element={<ResumePage />}></Route>
             {/* <Route path="/Misc"></Route> */}
+            <Route
+              path="/projects/CorporeSano"
+              element={<CorporeSanoPage />}
+            ></Route>
           </Routes>
         </Container>
       </Box>
