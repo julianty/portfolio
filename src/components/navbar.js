89dca2d8a07dab758469e8918a257b3961be57bd
julianty <alexanderjulianty@gmail.com>
2024-05-06 13:36:06 -0700

    Adds a color scheme toggle

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,15 +4,20 @@ import {
   Group,
   Anchor,
   useComputedColorScheme,
+  useMantineColorScheme,
+  Switch,
 } from "@mantine/core";
 
 import { Burger, Menu } from "@mantine/core";
-// import { useDisclosure } from "@mantine/hooks";
+import { IconMoon, IconSun } from "@tabler/icons-react";
 function MantineNavbar() {
-  // const [opened, { toggle }] = useDisclosure();
-  const colorScheme = useComputedColorScheme("dark", {
+  const colorScheme = useComputedColorScheme("light", {
     getInitialValueInEffect: true,
   });
+  const { setColorScheme } = useMantineColorScheme();
+  const toggleColorScheme = () => {
+    setColorScheme(colorScheme === "light" ? "dark" : "light");
+  };
   return (
     <Box>
       <Group m="md" justify="space-between">
@@ -25,28 +30,40 @@ function MantineNavbar() {
             julianty
           </Text>
         </Anchor>
-        <Menu>
-          <Menu.Target>
-            <Burger opened={false} />
-          </Menu.Target>
-          <Menu.Dropdown>
-            <Menu.Label>Navigation</Menu.Label>
-            <Menu.Item component="a" href="/">
-              Home
-            </Menu.Item>
-            <Menu.Item component="a" href="/Portfolio">
-              Portfolio
-            </Menu.Item>
-            <Menu.Item component="a" href="/Resume">
-              Resume
-            </Menu.Item>
-            <Menu.Divider />
-            <Menu.Label>Projects</Menu.Label>
-            <Menu.Item component="a" href="/projects/CorporeSano">
-              Corpore Sano
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
+        <Group>
+          <Switch
+            // label={colorScheme === "light" ? "Dark Mode" : "Light Mode"}
+            size="lg"
+            onLabel={<IconSun />}
+            offLabel={<IconMoon />}
+            onChange={toggleColorScheme}
+          />
+          <Menu>
+            <Menu.Target>
+              <Burger opened={false} />
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Label>Navigation</Menu.Label>
+              <Menu.Item component="a" href="/">
+                Home
+              </Menu.Item>
+              <Menu.Item component="a" href="/Portfolio">
+                Portfolio
+              </Menu.Item>
+              <Menu.Item component="a" href="/Resume">
+                Resume
+              </Menu.Item>
+              <Menu.Divider />
+              <Menu.Label>Projects</Menu.Label>
+              <Menu.Item component="a" href="/projects/CorporeSano">
+                Corpore Sano
+              </Menu.Item>
+              <Menu.Item component="a" href="/projects/SentencePerDay">
+                Sentence Per Day
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </Group>
       </Group>
     </Box>
   );
