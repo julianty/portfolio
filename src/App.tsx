9473e23d9775b872b3db1e243338f9ecf0ba0ff6
julianty <alexanderjulianty@gmail.com>
2025-01-03 15:34:42 -0800

    Implement routing and restructure App component with HomePage and ProjectHighlight

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,74 @@
-import Navbar from "./components/navbar";
-import { ThemeProvider } from "./components/theme-provider";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import ProjectPage from "./components/ProjectPage";
+import SkillBadge from "./components/skillBadge";
+import { Project, projectData } from "./projectData";
 
+import { ThemeProvider } from "./components/theme-provider";
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div id="main-stack" className="flex flex-col ">
-        <Navbar />
-        <section className="flex flex-col mx-auto my-48">
-          <h1>Hi, I'm Julian</h1>
-          <h4>Full Stack Developer</h4>
-        </section>
-        <section className="p-4">
-          <h2 className="">What I've been working on</h2>
-          <div className="flex flex-col md:flex-row">
-            <div className="bg-card p-4 rounded-lg">
-              <h3>Corpore Sano</h3>
-              <img
-                // src="Corpore Sano Project Highlight.png"
-                className="rounded-lg"
-                src="Corpore Sano Highlight.png"
-                alt="Corpore Sano"
-              />
-              <p>A lightweight workout tracking web application</p>
-            </div>
-            <div className="bg-card p-4 rounded-lg">
+      <Router>
+        <div id="main-stack" className="flex flex-col ">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/projects/CorporeSano"
+              element={<ProjectPage project={projectData[0]} />}
+            />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+function HomePage() {
+  return (
+    <>
+      <section className="hero flex flex-col mx-auto">
+        <h1>Hi, I'm Julian</h1>
+        <h4>Full Stack Developer</h4>
+      </section>
+      <section className="p-4">
+        <div className="flex items-center gap-2">
+          <h2 className="text-muted">What I've been working on</h2>
+          <hr className="flex-grow" />
+        </div>
+        <div className="flex flex-col">
+          <ProjectHighlight project={projectData[0]} />
+          {/* <div className="bg-card p-4 rounded-lg">
               <h3>Project 2</h3>
               <p>Project Description</p>
             </div>
             <div className="bg-card p-4 rounded-lg">
               <h3>Project 3</h3>
               <p>Project Description</p>
-            </div>
-          </div>
-        </section>
-      </div>
-      {/* <ColorTester /> */}
-      {/* <FloatingNav /> */}
-    </ThemeProvider>
+            </div> */}
+        </div>
+      </section>
+    </>
   );
 }
 
-function FloatingNav() {
+function ProjectHighlight({ project }: { project: Project }) {
   return (
-    <div className="absolute bottom-10 left-1/2 -translate-x-1/2">
-      <div className="bg-muted flex items-center gap-2 p-2 px-4 rounded-full">
-        <a className="p-3 px-4 rounded-full bg-muted-foreground" href="/">
-          Home
-        </a>
-        <a href="/">Projects</a>
-        <a href="/">Contact</a>
+    <div className="project-highlight">
+      <div className="flex justify-between">
+        <h3 className="project-title">{project.title}</h3>
+        <div className="flex gap-1">
+          {project.skills.map((skill) => (
+            <SkillBadge skill={skill} key={skill} />
+          ))}
+        </div>
       </div>
+      <p className="project-description">{project.description}</p>
+      <Link to={`projects/${project.link}`}>
+        <img className="rounded-lg" src={project.image} alt={project.title} />
+      </Link>
     </div>
   );
 }
-function ColorTester() {
-  const colors = [
-    "--foreground",
-    "--background",
-    "--card",
-    "--card-foreground",
-    "--primary",
-    "--primary-foreground",
-    "--secondary",
-    "--secondary-foreground",
-    "--muted",
-    "--muted-foreground",
-    "--accent",
-    "--accent-foreground",
-    "--destructive",
-    "--destructive-foreground",
-  ];
-  const divs = colors.map((color) => {
-    return (
-      <div
-        className="border-4 w-24 h-24 text-xs flex items-center justify-center text-blue-900"
-        style={{ backgroundColor: "hsl(var(" + color + "))" }}
-        key={color}
-      >
-        {" "}
-        {color}
-      </div>
-    );
-  });
-  return (
-    <div className="flex">
-      <div className="flex gap-4 flex-wrap">{divs}</div>
-    </div>
-  );
-}
+
 export default App;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center p-4">
-      <div className="text-2xl font-bold">julianty</div>
+      <div className="text-2xl font-bold">
+        <Link to="/">julianty</Link>
+      </div>
       <div className="flex gap-4">
-        <a href="#about">About</a>
+        <Link to="/about">About</Link>
         <a href="#projects">Projects</a>
         <a href="#contact">Contact</a>
       </div>
