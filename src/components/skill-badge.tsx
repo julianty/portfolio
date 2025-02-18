46c5d623c2bb4e793320c596bf4cb5bb4c38b031
julianty <alexanderjulianty@gmail.com>
2025-02-18 14:33:35 -0800

    Redesign skill badges to include logos

--- src/components/skill-badge.tsx
+++ src/components/skill-badge.tsx
@@ -1,4 +1,38 @@
 import { Badge } from "@/components/ui/badge";
+import {
+  IconBrandJavascript,
+  IconBrandTypescript,
+  IconBrandReact,
+  IconBrandNodejs,
+  IconBrandMongodb,
+  IconBrandHtml5,
+  IconBrandCss3,
+  IconBrandTailwind,
+  IconBrandBootstrap,
+  IconBrandVite,
+  IconBrandFirebase,
+  IconBrandSupabase,
+  IconBrandNextjs,
+} from "@tabler/icons-react";
+import PostgresqlSVG from "@/assets/PostgreSQL_logo.1.svg";
+const iconMap: Record<string, React.ReactNode> = {
+  JavaScript: <IconBrandJavascript />,
+  TypeScript: <IconBrandTypescript />,
+  React: <IconBrandReact />,
+  "Node.js": <IconBrandNodejs />,
+  Express: <>ex</>,
+  MongoDB: <IconBrandMongodb />,
+  HTML: <IconBrandHtml5 />,
+  CSS: <IconBrandCss3 />,
+  Tailwind: <IconBrandTailwind />,
+  Bootstrap: <IconBrandBootstrap />,
+  "Material-UI": <>mui</>,
+  Vite: <IconBrandVite />,
+  Firebase: <IconBrandFirebase />,
+  PostgreSQL: <img src={PostgresqlSVG} className="h-5"></img>,
+  Supabase: <IconBrandSupabase />,
+  "Next.js": <IconBrandNextjs />,
+};
 
 const skillMap: Record<string, string> = {
   JavaScript: "yellow",
@@ -23,6 +57,15 @@ export default function SkillBadge({ skill }: { skill: string }) {
   if (!skillMap[skill]) {
     return <Badge>{skill}</Badge>;
   } else {
-    return <Badge style={{ backgroundColor: skillMap[skill] }}>{skill}</Badge>;
+    return (
+      <Badge
+        style={{ backgroundColor: skillMap[skill] }}
+        className="flex gap-1"
+      >
+        {iconMap[skill]}
+
+        {skill}
+      </Badge>
+    );
   }
 }
