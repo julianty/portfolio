9eb2626147ab64483ba80e1cb049b02e348ef2b4
julianty <alexanderjulianty@gmail.com>
2025-02-20 13:28:33 -0800

    Update section header styling

--- src/components/project-page.tsx
+++ src/components/project-page.tsx
@@ -1,22 +1,30 @@
 import { Project } from "@/project-data";
 import { CodeBlock, dracula } from "react-code-blocks";
+import SkillBadge from "./skill-badge";
+import SectionHeader from "./ui/section-header";
 const ProjectPage = ({ project }: { project: Project }) => {
   return (
     <div className="max-w-4xl mx-auto p-6 min-h-screen">
       {/* Project Overview */}
       <section className="mb-10">
-        <h1 className="text-4xl font-bold text-foreground mb-4">
-          {project.title}
-        </h1>
-        <p className="text-lg text-secondary-foreground">
+        <div className="flex gap-4 items-center">
+          <h1 className="text-4xl font-bold text-foreground mb-4">
+            {project.title}
+          </h1>
+          <div className="flex gap-2">
+            {project.technologies &&
+              project.technologies.map((tech) => (
+                <SkillBadge key={tech} skill={tech} />
+              ))}
+          </div>
+        </div>
+        <p className="text-lg text-secondary-foreground ml-8">
           {project.longDescription}
         </p>
       </section>
       {/* Source Code and Demo Links */}
       <section className="mb-10">
-        <h2 className="text-2xl font-semibold text-foreground mb-4">
-          Source Code & Demo
-        </h2>
+        <SectionHeader>Source Code & Demo</SectionHeader>
         <div className="flex space-x-4">
           <a
             target="_blank"
@@ -37,31 +45,11 @@ const ProjectPage = ({ project }: { project: Project }) => {
       {/* Key Features */}
       {project.keyFeatures && project.keyFeatures.length > 0 && (
         <section className="mb-10">
-          <h2 className="text-2xl font-semibold text-foreground mb-4">
-            Key Features
-          </h2>
-          <ul className="list-disc list-inside space-y-2">
+          <SectionHeader>Key Features</SectionHeader>
+          <ul className="list-disc list-inside space-y-2 ml-8">
             {project.keyFeatures.map((feature) => (
               <li key={feature} className="text-secondary-foreground">
                 {feature}
-              </li>
-            ))}
-          </ul>
-        </section>
-      )}
-      {/* Technologies Used */}
-      {project.technologies && project.technologies.length > 0 && (
-        <section className="mb-10">
-          <h2 className="text-2xl font-semibold text-foreground mb-4">
-            Technologies Used
-          </h2>
-          <ul className="list-disc list-inside space-y-2">
-            {/* <li className="text-secondary-foreground">React</li>
-          <li className="text-secondary-foreground">Tailwind CSS</li>
-          <li className="text-secondary-foreground">Other Libraries/Frameworks</li> */}
-            {project.technologies.map((tech) => (
-              <li key={tech} className="text-secondary-foreground">
-                {tech}
               </li>
             ))}
           </ul>
@@ -72,13 +60,14 @@ const ProjectPage = ({ project }: { project: Project }) => {
         project.codeCommentaries &&
         project.code.length == project.codeCommentaries.length && (
           <section className="mb-10">
-            <h2 className="text-2xl font-semibold text-foreground mb-4">
+            {/* <h2 className="text-2xl font-semibold text-foreground mb-4">
               Code Highlights & Design Choices
-            </h2>
+            </h2> */}
+            <SectionHeader>Code Highlights & Design Choices</SectionHeader>
             {project.code.map((code, index) => {
               return (
-                <div key={index}>
-                  <div className="codeblock-wrapper">
+                <div key={index} className="flex flex-col gap-4">
+                  <div className="codeblock-wrapper ml-8">
                     <CodeBlock
                       text={code}
                       language="jsx"
@@ -87,7 +76,7 @@ const ProjectPage = ({ project }: { project: Project }) => {
                       wrapLongLines={true}
                     />
                   </div>
-                  <p className="text-secondary-foreground">
+                  <p className="text-secondary-foreground ml-8">
                     {project.codeCommentaries![index]}
                   </p>
                 </div>
@@ -98,10 +87,8 @@ const ProjectPage = ({ project }: { project: Project }) => {
       {/* Process and Workflow */}
       {project.process && (
         <section className="mb-10">
-          <h2 className="text-2xl font-semibold text-foreground mb-4">
-            Process and Workflow
-          </h2>
-          <p className="text-secondary-foreground whitespace-pre-line">
+          <SectionHeader>Process & Workflow</SectionHeader>
+          <p className="text-secondary-foreground whitespace-pre-line ml-8">
             {project.process}
           </p>
         </section>
@@ -109,31 +96,26 @@ const ProjectPage = ({ project }: { project: Project }) => {
       {/* Challenges and Solutions */}
       {project.challenges && (
         <section className="mb-10">
-          <h2 className="text-2xl font-semibold text-foreground mb-4">
-            Challenges and Solutions
-          </h2>
-          <p>{project.challenges[0].challenge}</p>
-          <p>{project.challenges[0].solution}</p>
-          {/* <p className="text-secondary-foreground">
-          Describe any challenges you faced during development and how you
-          solved them. This could include technical problems or unexpected
-          changes.
-        </p> */}
+          <SectionHeader>Challenges & Solutions</SectionHeader>
+          {project.challenges.map((challenge) => (
+            <div key={challenge.challenge} className="flex flex-col gap-4">
+              <p className="ml-8">{challenge.challenge}</p>
+              <p className="ml-12">{challenge.solution}</p>
+            </div>
+          ))}
         </section>
       )}
       {/* Visuals */}
       {project.demo && (
         <section className="mb-10">
-          <h2 className="text-2xl font-semibold text-foreground mb-4">
-            Screenshots & Demos
-          </h2>
+          <SectionHeader>Screenshots & Demos</SectionHeader>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
             {project.demo.map((source) => (
               <img
                 key={source}
                 src={`${source}`}
                 // alt="Screenshot"
-                className="rounded-lg shadow-md"
+                className="rounded-lg shadow-md max-h-[500px]"
               />
             ))}
           </div>
@@ -143,20 +125,18 @@ const ProjectPage = ({ project }: { project: Project }) => {
       {/* Outcomes and Impact */}
       {project.outcomes && (
         <section className="mb-10">
-          <h2 className="text-2xl font-semibold text-foreground mb-4">
-            Outcomes and Impact
-          </h2>
-          <p className="text-secondary-foreground">{project.outcomes}</p>
+          <SectionHeader>Outcomes & Impact</SectionHeader>
+          <p className="text-secondary-foreground ml-8">{project.outcomes}</p>
         </section>
       )}
 
       {/* What You Learned */}
       {project.whatILearned && (
         <section className="mb-10">
-          <h2 className="text-2xl font-semibold text-foreground mb-4">
-            What I Learned
-          </h2>
-          <p className="text-secondary-foreground">{project.whatILearned}</p>
+          <SectionHeader>What I Learned</SectionHeader>
+          <p className="text-secondary-foreground ml-8">
+            {project.whatILearned}
+          </p>
         </section>
       )}
     </div>
