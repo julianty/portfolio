e9a8dee67bf2efdcc0cba106d805dc1bcbb56f35
julianty <alexanderjulianty@gmail.com>
2025-07-04 15:34:43 -0700

    Adjust styling for mobile

--- src/home-page.tsx
+++ src/home-page.tsx
@@ -73,9 +73,9 @@ export default function HomePage() {
       </section>
       <section id="skills">
         <SectionHeader>Skills</SectionHeader>
-        <div id="skills" className="flex flex-col gap-8 w-3/4 mx-auto">
+        <div id="skills" className="flex flex-col gap-8 w-5/6 md:w-3/4 mx-auto">
           Languages
-          <div className="flex flex-wrap gap-2 ml-8">
+          <div className="flex flex-wrap gap-2 md:ml-8">
             <SkillBadge skill="HTML" />
             <SkillBadge skill="CSS" />
             <SkillBadge skill="JavaScript" />
@@ -83,14 +83,14 @@ export default function HomePage() {
             <SkillBadge skill="Python" />
           </div>
           Front End Technologies
-          <div className="flex flex-wrap gap-2 ml-8">
+          <div className="flex flex-wrap gap-2 md:ml-8">
             <SkillBadge skill="React" />
             <SkillBadge skill="Tailwind" />
             <SkillBadge skill="Bootstrap" />
             <SkillBadge skill="Vite" />
           </div>
           Back End Technologies
-          <div className="flex flex-wrap gap-2 ml-8">
+          <div className="flex flex-wrap gap-2 md:ml-8">
             <SkillBadge skill="Express" />
             <SkillBadge skill="Node.js" />
             <SkillBadge skill="MongoDB" />
--- src/project-highlight.tsx
+++ src/project-highlight.tsx
@@ -8,7 +8,7 @@ export default function ProjectHighlight({ project }: { project: Project }) {
       <Link to={`projects/${project.pageLink}`}>
         <div className="flex relative group justify-center items-center">
           <img
-            className="rounded-lg group-hover:opacity-50 max-w-[87%]"
+            className="rounded-lg group-hover:opacity-50"
             src={project.image ? project.image : placeholderImage}
             alt={project.title}
           />
@@ -17,9 +17,11 @@ export default function ProjectHighlight({ project }: { project: Project }) {
           </div>
         </div>
       </Link>
-      <div className="flex justify-between flex-wrap">
-        <h3 className="project-title">{project.title}</h3>
-        <div className="flex gap-1">
+      <div className="flex flex-col gap-3 justify-between flex-wrap p-2">
+        <Link to={`projects/${project.pageLink}`}>
+          <h3 className="project-title">{project.title}</h3>
+        </Link>
+        <div className="flex gap-1 flex-wrap">
           {project.skills.map((skill) => (
             <SkillBadge skill={skill} key={skill} />
           ))}
