75a16518caf44d57ae47b82027b34c5db8624ab7
julianty <alexanderjulianty@gmail.com>
2025-09-09 11:31:06 -0700

    Add background gradient to hero

--- src/home-page.tsx
+++ src/home-page.tsx
@@ -9,7 +9,7 @@ import { Button } from "./components/ui/button";
 export default function HomePage() {
   return (
     <>
-      <section className="h-[90vh] flex flex-col justify-center items-center bg-linear-to-tr from-background to-foreground">
+      <section className="h-[90vh] flex flex-col justify-center items-center bg-radial from-slate-800 to-70% to-background">
         <h1>Hi, I'm Julian</h1>
         <h4>Full Stack Developer</h4>
       </section>
