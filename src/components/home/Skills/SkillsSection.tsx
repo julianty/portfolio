18a526d90749ab3f9821f279457b5219cef5c150
julianty <alexanderjulianty@gmail.com>
2025-11-24 07:57:34 -0800

    Add icon to some skills

--- src/components/home/Skills/SkillsSection.tsx
+++ src/components/home/Skills/SkillsSection.tsx
@@ -1,5 +1,10 @@
 import SectionHeader from "@/components/ui/section-header";
 import SkillGroup from "./SkillGroup";
+import {
+  IconBracketsAngle,
+  IconDevices,
+  IconStack2,
+} from "@tabler/icons-react";
 
 function SkillsSection() {
   return (
@@ -7,15 +12,17 @@ function SkillsSection() {
       <SectionHeader>Skills</SectionHeader>
       <div
         id="skills"
-        className="flex flex-col gap-2 md:gap-8 w-5/6 md:w-3/4 md:flex-row mx-auto"
+        className="flex flex-col gap-2 md:gap-8 w-5/6 md:w-3/4 mx-auto"
       >
         <SkillGroup
           title="Programming Languages"
           skills={["HTML", "CSS", "JavaScript", "TypeScript", "Python"]}
+          icon={<IconBracketsAngle />}
         />
         <SkillGroup
           title="Front End Technologies"
           skills={["React", "Tailwind", "Bootstrap", "Vite"]}
+          icon={<IconDevices />}
         />
         <SkillGroup
           title="Back End Technologies"
@@ -27,6 +34,7 @@ function SkillsSection() {
             "Firebase",
             "Supabase",
           ]}
+          icon={<IconStack2 />}
         />
       </div>
     </section>
