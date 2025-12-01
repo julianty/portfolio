db9e9963a0ac2e494ec434584253c00a59b1789a
julianty <alexanderjulianty@gmail.com>
2025-12-01 09:18:58 -0800

    Add additional prompt

--- src/components/home/chatbox/prewrittenPrompts.ts
+++ src/components/home/chatbox/prewrittenPrompts.ts
@@ -1,7 +1,13 @@
-const prewrittenPrompts = [
+const prewrittenPrompts: prompt[] = [
   { text: "What is Julian's experience with (React)?" },
+  { text: "Tell me about Julian." },
 ];
+
+// Set each to active by default
+prewrittenPrompts.forEach((p) => (p.active = true));
+
 export default prewrittenPrompts;
 export type prompt = {
   text: string;
+  active?: boolean;
 };
