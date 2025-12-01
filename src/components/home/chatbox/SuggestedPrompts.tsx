b1e980b72c6cd278c3af10bbe5e26843abebd373
julianty <alexanderjulianty@gmail.com>
2025-12-01 09:18:46 -0800

    On click, suggested prompts are removed

--- src/components/home/chatbox/SuggestedPrompts.tsx
+++ src/components/home/chatbox/SuggestedPrompts.tsx
@@ -13,21 +13,34 @@ function SuggestedPrompts({
   React.useEffect(() => {
     setPrompts(prewrittenPrompts);
   }, []);
+
+  // On mouseDown handler
+  const handleMouseDown = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    p: prompt
+  ) => {
+    e.preventDefault();
+    // Mark prompt as inactive
+    p.active = false;
+    // Set input text
+    setInput(p.text);
+  };
+
   return (
     <div className="flex justify-end">
-      {prompts.map((p) => (
-        <div
-          className="p-2 rounded-xl bg-muted text-foreground flex gap-1 text-sm items-center"
-          key={p.text}
-          onMouseDown={(e) => {
-            e.preventDefault();
-            setInput(p.text);
-          }}
-        >
-          <IconSparkles />
-          {p.text}
-        </div>
-      ))}
+      {prompts.map((p) => {
+        if (!p.active) return;
+        return (
+          <div
+            className="p-2 rounded-xl bg-muted text-foreground flex gap-1 text-sm items-center"
+            key={p.text}
+            onMouseDown={(e) => handleMouseDown(e, p)}
+          >
+            <IconSparkles />
+            {p.text}
+          </div>
+        );
+      })}
     </div>
   );
 }
